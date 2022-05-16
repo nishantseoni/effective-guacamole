d40c034aad08ece05518603b6d22f303649d8fa3
Nishant <nishant@fucku.com>
2022-05-16 13:23:03 -0700

    added bash script

---
+++ buildsite.sh
@@ -0,0 +1,8 @@
+cat templates/top.html content/index.html templates/bottom.html > index.html 
+mv index.html docs/
+cat templates/top.html content/about.html templates/bottom.html > about.html 
+mv about.html docs/
+cat templates/top.html content/contact.html templates/bottom.html > contact.html 
+mv contact.html docs/
+cat templates/top.html content/portfolio.html templates/bottom.html > portfolio.html 
+mv portfolio.html docs/
